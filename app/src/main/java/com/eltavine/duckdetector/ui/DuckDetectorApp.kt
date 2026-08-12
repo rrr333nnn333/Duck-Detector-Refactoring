1a51eb13091a638179dd18f7c3934ec1b4bf8fd0
Rene <rrr333nnn333@gmail.com>
2026-08-12 13:57:53 -0400

    drop: AlphaBuildBanner

--- app/src/main/java/com/eltavine/duckdetector/ui/DuckDetectorApp.kt
+++ app/src/main/java/com/eltavine/duckdetector/ui/DuckDetectorApp.kt
@@ -64,7 +64,7 @@ import com.eltavine.duckdetector.core.packagevisibility.preferences.PackageVisib
 import com.eltavine.duckdetector.core.startup.legal.AgreementAcceptancePrefs
 import com.eltavine.duckdetector.core.startup.legal.AgreementAcceptanceStore
 // import com.eltavine.duckdetector.core.startup.legal.AgreementScreen
-import com.eltavine.duckdetector.core.ui.components.AlphaBuildBanner
+// import com.eltavine.duckdetector.core.ui.components.AlphaBuildBanner
 // import com.eltavine.duckdetector.core.ui.components.AlphaBuildWarningOverlay
 import com.eltavine.duckdetector.core.ui.components.DetectorAutoExpansionDirective
 import com.eltavine.duckdetector.core.ui.components.LocalDetectorAutoExpansionDirective
@@ -395,9 +395,9 @@ fun DuckDetectorApp() {
 
             ScreenshotWatermarkOverlay()
 
-            if (agreementAccepted && startupPoliciesReady) {
-                AlphaBuildBanner()
-            }
+            // if (agreementAccepted && startupPoliciesReady) {
+            //     AlphaBuildBanner()
+            // }
 
             // AlphaBuildWarningOverlay(
             //     forceVisible = agreementAccepted &&
